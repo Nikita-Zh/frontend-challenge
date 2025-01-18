d50632169384946d5a0cd30dce997a7a7844d308
unknown <n.d.2002z@gmail.com>
2025-01-18 19:09:17 +0300

    (fix) action script

--- kitty_pinterest/next.config.ts
+++ kitty_pinterest/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  basePath: "frontend-challenge",
+  output: "export",
   images: {
     remotePatterns: [
       {
