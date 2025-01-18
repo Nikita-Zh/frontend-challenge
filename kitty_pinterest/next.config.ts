7b2df1ce21f53c76b5d90152e8290aee4f505d34
unknown <n.d.2002z@gmail.com>
2025-01-18 20:11:12 +0300

    (deploy) added gh-pages script

--- kitty_pinterest/next.config.ts
+++ kitty_pinterest/next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   // basePath: "frontend-challenge",
+  // output: "export",
+
+  reactStrictMode: true,
+  assetPrefix: isProd ? "/frontend-challenge/" : "",
+  basePath: isProd ? "/frontend-challenge" : "",
   output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "cdn2.thecatapi.com",
